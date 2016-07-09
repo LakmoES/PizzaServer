1ac055036b50da6448e81268acbd51b1673e6aa5
Maxim Shmelyov <mokinous@gmail.com>
2016-07-09 17:13:00 +0300

    NoMoreGuest function is done.

--- Pizza/Controllers/AuthController.cs
+++ Pizza/Controllers/AuthController.cs
@@ -22,7 +22,7 @@ namespace Pizza.Controllers
             if (username == null || password == null)
                 return Json("false", JsonRequestBehavior.AllowGet);
 
-            var userList = dbContext.Users.Where(u => u.username == username.ToLower()).ToList();
+            var userList = dbContext.Users.Where(u => u.username.ToLower() == username.ToLower()).ToList();
             if (userList.Count == 0)
                 return Json("user not found", JsonRequestBehavior.AllowGet);
             if (userList.First().password == password)
@@ -61,7 +61,7 @@ namespace Pizza.Controllers
         public JsonResult RegisterUser(string username, string password, string email, string name, string surname)
         {
             User user = new User();
-            user.username = username == null ? null : username.ToLower();
+            user.username = username;
             user.password = password;
             user.email = email == null ? null : email.ToLower();
             user.name = name;
@@ -71,7 +71,7 @@ namespace Pizza.Controllers
             List<Error> errors;
             if (UserValidator.CheckAdditionUser(user, out errors))
             {
-                var foundUser = dbContext.Users.Where(x => x.username == user.username).ToList();
+                var foundUser = dbContext.Users.Where(x => x.username.ToLower() == user.username.ToLower()).ToList();
                 if (foundUser.Count > 0)
                 {
                     errors.Add(new Error { error = "Пользователь с таким именем уже существует" });
@@ -134,6 +134,64 @@ namespace Pizza.Controllers
                 return Json(errors, JsonRequestBehavior.AllowGet);
             else
                 return Json(new { username = guest.username, password = guest.password }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult NoMoreGuest(string token, string username, string password, string email, string name, string surname)
+        {
+            if (!AuthProvider.Instance.CheckToken(dbContext, token))
+                return Json("wrong token", JsonRequestBehavior.AllowGet);
+
+            int userID = dbContext.Tokens.Find(token).user;
+            if (dbContext.Users.Find(userID).guest != 1)
+                return Json("not a guest", JsonRequestBehavior.AllowGet);
+
+            User updatedUser = new User();
+            updatedUser.username = username;
+            updatedUser.password = password;
+            updatedUser.email = email == null ? null : email.ToLower();
+            updatedUser.name = name;
+            updatedUser.surname = surname;
+            updatedUser.guest = 0;
+
+            List<Error> errors;
+            if (UserValidator.CheckAdditionUser(updatedUser, out errors))
+            {
+                var foundUser = dbContext.Users.Where(x => x.username.ToLower() == updatedUser.username.ToLower()).ToList();
+                if (foundUser.Count > 0)
+                {
+                    errors.Add(new Error { error = "Пользователь с таким именем уже существует" });
+                }
+                else
+                {
+                    var user = dbContext.Users.Find(userID);
+                    dbContext.Users.Attach(user);
+                    var entry = dbContext.Entry(user);
+
+                    entry.Property(e => e.username).IsModified = true;
+                    entry.Entity.username = updatedUser.username;
+
+                    entry.Property(e => e.password).IsModified = true;
+                    entry.Entity.password = updatedUser.password;
+
+                    entry.Property(e => e.email).IsModified = true;
+                    entry.Entity.email = updatedUser.email;
+
+                    entry.Property(e => e.name).IsModified = true;
+                    entry.Entity.name = updatedUser.name;
+
+                    entry.Property(e => e.surname).IsModified = true;
+                    entry.Entity.surname = updatedUser.surname;
+
+                    entry.Property(e => e.guest).IsModified = true;
+                    entry.Entity.guest = updatedUser.guest;
+
+                    dbContext.SaveChanges();
+                }
+            }
+
+            if (errors.Count > 0)
+                return Json(errors, JsonRequestBehavior.AllowGet);
+            else
+                return Json("ok", JsonRequestBehavior.AllowGet);
         }
         private string RandomString(int length)
         {
