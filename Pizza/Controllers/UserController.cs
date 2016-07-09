94049cef5cda62c6d7aa6b95f4ab997bb463a0f4
Maxim Shmelyov <mokinous@gmail.com>
2016-07-09 13:11:28 +0300

    Tel numbers and addresses. EDIT|REMOVE

--- Pizza/Controllers/UserController.cs
+++ Pizza/Controllers/UserController.cs
@@ -60,15 +60,6 @@ namespace Pizza.Controllers
 
             return Json("ok", JsonRequestBehavior.AllowGet);
         }
-        public JsonResult ShowAll(string token)
-        {
-            if (!AuthProvider.Instance.CheckToken(dbContext, token))
-                return Json("wrong token", JsonRequestBehavior.AllowGet);
-
-            IEnumerable<User> users = dbContext.Users;
-
-            return Json(users, JsonRequestBehavior.AllowGet);
-        }
         public JsonResult AddTel(string token, string tel)
         {
             if (!AuthProvider.Instance.CheckToken(dbContext, token))
@@ -112,6 +103,84 @@ namespace Pizza.Controllers
             var addressList = dbContext.UserAddress.Where(ua => ua.user == t.user).Select(ua => new { id = ua.id, address = ua.address }).ToList();
 
             return Json(addressList, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult RemoveTel(string token, int? telID)
+        {
+            if (!AuthProvider.Instance.CheckToken(dbContext, token))
+                return Json("wrong token", JsonRequestBehavior.AllowGet);
+            if (telID == null)
+                return Json("bad argument", JsonRequestBehavior.AllowGet);
+
+            var t = dbContext.Tokens.Find(token);
+            dbContext.UserTelephones.RemoveRange(dbContext.UserTelephones.Where(tel => tel.user == t.user && tel.id == telID));
+            dbContext.SaveChanges();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult RemoveAddress(string token, int? addressID)
+        {
+            if (!AuthProvider.Instance.CheckToken(dbContext, token))
+                return Json("wrong token", JsonRequestBehavior.AllowGet);
+            if (addressID == null)
+                return Json("bad argument", JsonRequestBehavior.AllowGet);
+
+            var t = dbContext.Tokens.Find(token);
+            dbContext.UserAddress.RemoveRange(dbContext.UserAddress.Where(address => address.user == t.user && address.id == addressID));
+            dbContext.SaveChanges();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult EditTel(string token, int? telID, string tel)
+        {
+            if (!AuthProvider.Instance.CheckToken(dbContext, token))
+                return Json("wrong token", JsonRequestBehavior.AllowGet);
+            if (telID == null || tel == null)
+                return Json("bad argument", JsonRequestBehavior.AllowGet);
+            if(tel.Length < 1)
+                return Json("bad argument", JsonRequestBehavior.AllowGet);
+
+            var userID = dbContext.Tokens.Find(token).user;
+            var telephone = dbContext.UserTelephones.FirstOrDefault(t => t.id == telID && t.user == userID);
+            if (telephone == null)
+                return Json("telephone not found", JsonRequestBehavior.AllowGet);
+
+            var updatedTel = new UserTelephone { number = tel };
+
+            dbContext.UserTelephones.Attach(telephone);
+            var entry = dbContext.Entry(telephone);
+
+            entry.Property(e => e.number).IsModified = true;
+            entry.Entity.number = updatedTel.number;
+            
+            dbContext.SaveChanges();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult EditAddress(string token, int? addressID, string address)
+        {
+            if (!AuthProvider.Instance.CheckToken(dbContext, token))
+                return Json("wrong token", JsonRequestBehavior.AllowGet);
+            if (addressID == null || address == null)
+                return Json("bad argument", JsonRequestBehavior.AllowGet);
+            if (address.Length < 1)
+                return Json("bad argument", JsonRequestBehavior.AllowGet);
+
+            var userID = dbContext.Tokens.Find(token).user;
+            var addr = dbContext.UserAddress.FirstOrDefault(a => a.id == addressID && a.user == userID);
+            if (addr == null)
+                return Json("address not found", JsonRequestBehavior.AllowGet);
+
+            var updatedAddress = new UserAddress { address = address };
+
+            dbContext.UserAddress.Attach(addr);
+            var entry = dbContext.Entry(addr);
+
+            entry.Property(e => e.address).IsModified = true;
+            entry.Entity.address = updatedAddress.address;
+
+            dbContext.SaveChanges();
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
         }
     }
 }
