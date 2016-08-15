d3879225cc511b1a6bf7be2a04f3e6b7c625f6bb
Maxim Shmelyov <mokinous@gmail.com>
2016-08-15 11:26:25 +0300

    NoMoreGuest bug fix

--- Pizza/Controllers/AuthController.cs
+++ Pizza/Controllers/AuthController.cs
@@ -168,7 +168,7 @@ namespace Pizza.Controllers
                 {
                     errors.Add(new Error { error = "Пользователь с таким именем уже существует" });
                 }
-                foundUser = dbContext.Users.Where(x => x.email == updatedUser.email && x.id != updatedUser.id).ToList();
+                foundUser = dbContext.Users.Where(x => x.email == updatedUser.email && x.id != userID).ToList();
                 if (foundUser.Count > 0)
                     errors.Add(new Error { error = "Пользователь с таким email адресом уже существует" });
                 else
